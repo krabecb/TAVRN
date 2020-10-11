4ff3ec13d7f7863598c86336e6253579eb501102
krabecb <60526718+krabecb@users.noreply.github.com>
2020-10-10 19:58:49 -0500

    Created and set up server.js, .env, and updated .gitignore

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const server = express()
+const PORT = process.env.PORT
+
+//Middleware
+server.use(express.static('public'))
+
+server.listen(PORT, () => {
+	const d = new Date()
+	console.log(`${d.toLocaleString()}: Server is running on port: ${PORT}`)
+})
