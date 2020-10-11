73f45baa95742e5abc2b39a164152294257676c1
krabecb <60526718+krabecb@users.noreply.github.com>
2020-10-10 20:35:19 -0500

    Added '/' route to server.js

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const PORT = process.env.PORT
 //Middleware
 server.use(express.static('public'))
 
+//Controllers
+
+//Routes
+server.get('/', (req, res) => {
+	res.render('home.ejs')
+})
+
 server.listen(PORT, () => {
 	const d = new Date()
 	console.log(`${d.toLocaleString()}: Server is running on port: ${PORT}`)
