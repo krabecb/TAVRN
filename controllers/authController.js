b589e3eb3083212a75e320e98958f44b7a201000
krabecb <60526718+krabecb@users.noreply.github.com>
2020-11-22 20:30:34 -0600

    /auth/register POST works. Updated server.js to include sessions.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,62 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
 //Routes
 
-//Registration form
+//Registration form: GET /auth/register
 router.get('/register', (req, res) => {
 	res.render('auth/register.ejs')
+})
+
+//Registration form: POST /auth/register
+router.post('/register', async (req, res, next) => {
+	try {
+		console.log(req.body)
+		const requestedUsername = req.body.username
+		const requestedPassword = req.body.password
+		const userFirstName = req.body.firstname
+		const userLastName = req.body.lastname
+		const userDOB = req.body.date
+		const userHometown = req.body.hometown
+		const requestedEmail = req.body.email
+
+		const userWithThisUsername = await User.findOne({
+			username: requestedUsername
+		})
+		console.log("Here is userWithThisUsername: " + userWithThisUsername)
+
+		if(userWithThisUsername) {
+			console.log("username exists")
+			req.session.message = `Username ${requestedUsername} has already been taken.`
+			res.redirect('/auth/register')
+		} else {
+			const salt = bcrypt.genSaltSync(10)
+			const hashedPassword = bcrypt.hashSync(requestedPassword, salt)
+			const createdUser = await User.create({
+				username: requestedUsername,
+				password: hashedPassword,
+				firstName: userFirstName,
+				lastName: userLastName,
+				dateOfBirth: userDOB,
+				hometown: userHometown,
+				email: requestedEmail
+			})
+			req.session.loggedIn = true
+			req.session.userId = createdUser._id
+			req.session.username = createdUser.username
+			req.session.firstName = createdUser.firstName
+			req.session.lastName = createdUser.lastName
+			req.session.dateOfBirth = createdUser.dateOfBirth
+			req.session.hometown = createdUser.hometown
+			req.session.email = createdUser.email
+			req.session.message = `Thanks for signing up, ${createdUser.username}!`
+			res.redirect('/')
+		}
+	} catch(err) {
+		next(err)
+	}
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const server = express()
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const methodOverride = require('method-override')
 const PORT = process.env.PORT
 
 //Database
@@ -8,6 +11,28 @@ require('./db/db')
 
 //Middleware
 server.use(express.static('public'))
+server.use(bodyParser.urlencoded({ extended: false}))
+server.use(methodOverride('_method'))
+
+//Sessions
+server.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false
+}))
+
+server.use((req, res, next) => {
+	console.log("Here is the session in the custom app-level middleware.")
+	console.log(req.session)
+	res.locals.loggedIn = req.session.loggedIn
+	res.locals.username = req.session.username
+	res.locals.userId = req.session.userId
+	//flash messaging
+	res.locals.message = req.session.message
+	//clears out so it only appears once
+	req.session.message = undefined
+	next()
+})
 
 //Controllers
 const authController = require('./controllers/authController')
